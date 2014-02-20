a799bef6aeaab0dbedf29aca5c55b69f1ea32796
Douraid Arfaoui <douraid.arfaoui@gmail.com>
2014-02-20 01:45:36 +0100

    deletion for structure changing purposes

--- README.md
+++
@@ -1,4 +0,0 @@
-jrey
-====
-
-Java MVC framework for web applications with Apache Velocity implemented for templating
--- src/com/jrey/exceptions/NoSuchUrlParameterException.java
+++
@@ -1,9 +0,0 @@
-package com.jrey.exceptions;
-
-@SuppressWarnings("serial")
-public class NoSuchUrlParameterException extends Exception {
-	
-		public NoSuchUrlParameterException(String param){
-			super("The requested action URL does not contain a parameter with the following name:" + param );
-		}
-}
